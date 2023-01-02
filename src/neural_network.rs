46afcb4a2a618fd23a832c7c453934e82ab9ff00
driesdemaeseneer <dries.demaeseneer@student.uantwerpen.be>
2023-01-02 17:03:30 +0100

    Feat: neural network file

--- src/neural_network.rs
+++ src/neural_network.rs
@@ -1,5 +1,13 @@
+use ndarray::{Array, Array1, IxDyn};
+
+pub type Weights = Array1<f32>;
+pub type Biases = Array1<f32>;
+pub type Layer = (Weights, Biases);
+
 // Neural network implementations and traits
-pub struct NN {}
+pub struct NN<const AMT_LAYERS: usize> {
+    layers: Vec<Layer>,
+}
 // used to create valid neural networks,
 // make macro for layers?
 pub struct NNBuilder {}
